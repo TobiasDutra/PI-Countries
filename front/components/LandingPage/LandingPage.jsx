41a3870c9a576e89b128479a6bfb28d68b8fdccb
Tobias Dutra <tobias.dutra2009@gmail.com>
2024-12-13 05:36:27 -0300

    :D

--- front/components/LandingPage/LandingPage.jsx
+++ front/components/LandingPage/LandingPage.jsx
@@ -5,8 +5,10 @@ import style from './LandingPage.module.css'
 export default function LandingPage() {
     return (
         <div className={style.greet}>
-            <h2>Bienvenidos a mi PI de paises</h2>
-            <Link to="/home"><button>Ingresar</button></Link >
+            <div className={style.greett}>
+                <h2>Bienvenidos a mi PI de paises</h2>
+                <Link to="/home"><button>Ingresar</button></Link >
+            </div>
         </div>
     )
 }
--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,20 +1,18 @@
-import { Route , Routes} from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import LandingPage from "../components/LandingPage/LandingPage"
 import Home from '../components/HomePage/Home'
-import ActivityCreate  from '../components/ActivityCreate/ActivityCreate'
+import ActivityCreate from '../components/ActivityCreate/ActivityCreate'
 import Detail from '../components/Detail/Detail'
 
 
 function App() {
   return (
-    <div className="App"> 
-      <Routes> 
-        <Route path="" element={<LandingPage/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/activity" element={<ActivityCreate/>}/> 
-        <Route path="/home/:id" element={<Detail/>}/>
-      </Routes> 
-    </div>
+    <Routes>
+      <Route path="" element={<LandingPage />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/activity" element={<ActivityCreate />} />
+      <Route path="/home/:id" element={<Detail />} />
+    </Routes>
   )
 }
 
