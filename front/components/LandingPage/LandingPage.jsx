5b97cb3c28e3698e4ad5c2a08d14f68300b7c2a2
Tobias Dutra <tobias.dutra2009@gmail.com>
2024-12-13 05:43:28 -0300

    a

--- front/components/LandingPage/LandingPage.jsx
+++ front/components/LandingPage/LandingPage.jsx
@@ -8,6 +8,7 @@ export default function LandingPage() {
             <div className={style.greett}>
                 <h3>Bienvenidos a mi PI de paises</h3>
                 <Link to="/home"><button>Ingresar</button></Link >
+                <p>hola</p>
             </div>
         </div>
     )
